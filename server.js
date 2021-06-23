7792f554d13568ab6c7c40531469182b222a086f
abbasihsn <abbasi_ha@yahoo.com>
2021-06-23 20:33:13 +0430

    final final

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const cors = require('cors');
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : '95300718',
-      database : 'postgres'
+      connectionString : process.env.DATABASE_URL,
+      ssl:true
     }
   });
 
